64bb4884e00f2754a5356c1aaca89f87e26dcb81
TomTomDam <thomasdam97@gmail.com>
2022-11-02 18:43:20 +0000

    Added conditional rendering for Exercise pane if the workout exercise list is empty or not

--- lib/widgets/add_exercise_button.dart
+++ lib/widgets/add_exercise_button.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import '../screens/exercise_list/add_exercises.dart';
 
 class AddExerciseButton extends StatelessWidget {
-  const AddExerciseButton({Key? key}) : super(key: key);
+  const AddExerciseButton({Key? key, required this.width}) : super(key: key);
+  final double width;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
+      width: width,
       child: ElevatedButton(
-        child: const Text("+ Add Exercise"),
+        child: const Text("+ Add Exercise", textAlign: TextAlign.center),
         onPressed: () {
           Navigator.push(context,
               MaterialPageRoute(builder: (context) => const AddExercises()));
