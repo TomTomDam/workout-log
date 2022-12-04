918486f693af7410b9acb867a4f04053065e6fff
TomTomDam <thomasdam97@gmail.com>
2022-12-04 16:18:29 +0000

    Added repository. Still need to decide if i need to make a dedicated EFCoreRepository

---
+++ WorkoutLog.API/WorkoutLog.API/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutLog.API.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Insert(T entity);
+        Task Update(T entity);
+        Task Delete(int id);
+        Task Save();
+    }
+}
