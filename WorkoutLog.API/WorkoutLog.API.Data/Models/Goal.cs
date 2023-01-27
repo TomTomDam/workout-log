d9e86073fb26c5f78ea9acc27ac843a5d78826c1
TomTomDam <thomasdam97@gmail.com>
2023-01-27 21:01:32 +0000

    Finished model validation on int fields

--- WorkoutLog.API/WorkoutLog.API.Data/Models/Goal.cs
+++ WorkoutLog.API/WorkoutLog.API.Data/Models/Goal.cs
@@ -5,9 +5,11 @@ namespace WorkoutLog.API.Data.Models
     public class Goal : Entity
     {
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = $"The {nameof(CurrentValue)} field is required.")]
         public int CurrentValue { get; set; }
 
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = $"The {nameof(GoalValue)} field is required.")]
         public int GoalValue { get; set; }
 
         public DateTime DateCreated { get; set; }
