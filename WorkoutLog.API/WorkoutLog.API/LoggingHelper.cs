4b279e7c2b2a2ff2dba3ecee20f1645288182c5c
TomTomDam <thomasdam97@gmail.com>
2023-01-28 15:13:39 +0000

    Added Logging helper class using diagnostic context to append useful information such as url components and the name of the endpoint being called

---
+++ WorkoutLog.API/WorkoutLog.API/LoggingHelper.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+
+namespace WorkoutLog.API
+{
+    public static class LoggingHelper
+    {
+        public static void EnrichFromRequest(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+
+            // Set all the common properties available for every request
+            diagnosticContext.Set("Host", request.Host);
+            diagnosticContext.Set("Protocol", request.Protocol);
+            diagnosticContext.Set("Scheme", request.Scheme);
+
+            // Only set it if available. You're not sending sensitive data in a querystring right?!
+            if (request.QueryString.HasValue)
+            {
+                diagnosticContext.Set("QueryString", request.QueryString.Value);
+            }
+
+            diagnosticContext.Set("ContentType", httpContext.Response.ContentType);
+
+            var endpoint = httpContext.GetEndpoint();
+            if (endpoint != null && endpoint is object)
+                diagnosticContext.Set("EndpointName", endpoint.DisplayName);
+        }
+    }
+}
